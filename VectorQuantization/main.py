6e1898d692872c132d9fd75b0aabf7f7fbb6f05e
beshirr <youssefahmedbeshir@gmail.com>
2025-11-28 17:47:46 +0200

    main menu

--- VectorQuantization/main.py
+++ VectorQuantization/main.py
@@ -1,12 +1,60 @@
 from VectorQuantization import VectorQuantization
+import os
 
-VQ: VectorQuantization = VectorQuantization('RandomImage.jpg', 4, 2, 2)
+def encode():
+    while True:
+        imagePath = input("Enter image path: ")
+        if os.path.exists(imagePath):
+            break
+        print("File not found. Please try again.")
 
-VQ.Encode('EncodingStream.txt', 'CodeBook.txt')
+    while True:
+        try:
+            codeBookSize = int(input("Enter code book size: "))
+            if codeBookSize >= 2:
+                break
+            print("Code book size must be at least 2.")
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+
+    vectorHeight = int(input("Enter vector height: "))
+    vectorWidth = int(input("Enter vector width: "))
+    VQ: VectorQuantization = VectorQuantization(imagePath, codeBookSize, vectorHeight, vectorWidth)
+    VQ.Encode('EncodingStream.txt', 'CodeBook.txt')
 
 
+def decode():
+    while True:
+        codeBookFileName = input("Enter code book file name: ")
+        if os.path.exists(codeBookFileName):
+            break
+        print("File not found. Please try again.")
 
-img = VectorQuantization.Decode('CodeBook.txt','EncodingStream.txt')
-img.save('DecodedImage.jpg')
+    while True:
+        bitsStreamFileName = input("Enter bits stream file name: ")
+        if os.path.exists(bitsStreamFileName):
+            break
+        print("File not found. Please try again.")
+
+    img = VectorQuantization.Decode(codeBookFileName, bitsStreamFileName)
+    img.save('DecodedImage.jpg')
 
 
+def main():
+    while True:
+        print("Vector Quantization Compression")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Exit")
+        choice = input("Enter your choice: ")
+
+        if (choice == "1"):
+            encode()
+        elif (choice == "2"):
+            decode()
+        elif (choice == "3"):
+            break
+
+
+if __name__ == "__main__":
+    main()
